9089038e05473e62d2c8755bf11e96ed05558fa6
jd4rider <jd4rider@gmail.com>
2023-02-05 02:06:19 -0700

    yay

--- api/index.go
+++ api/index.go
@@ -1,33 +1,10 @@
-package main
+package handler
 
 import (
 	"fmt"
-	"github.com/gofiber/fiber/v2"
+	"net/http"
 )
 
-//func Main() {
-//	app := fiber.New()
-//
-//	app.Get("/", func(c *fiber.Ctx) error {
-//		return c.SendString("Hello, World!")
-//	})
-//
-//	app.Listen(":3000")
-//}
-
-func HelloWorld(c *fiber.Ctx) error {
-	return c.SendString("Hello, World!")
-}
-
-func SetupRoutes(app *fiber.App) {
-	app.Get("/", HelloWorld)
-}
-
-func main() {
-	app := fiber.New()
-	SetupRoutes(app)
-	err := app.Listen(":3000")
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
