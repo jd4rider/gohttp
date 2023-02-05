0420d2db429b8f1ddb3ffb868983a5b4be8cd255
jd4rider <jd4rider@gmail.com>
2023-02-05 01:45:11 -0700

    i dunno

--- api/index.go
+++ api/index.go
@@ -1,10 +1,13 @@
-package handler
+package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "github.com/gofiber/fiber/v2"
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(":3000")
 }
